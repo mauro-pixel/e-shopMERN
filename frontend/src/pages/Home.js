82f6bad983e59886d540feda23b8f29faa327b83
Mauro <mauro@Mauros-MacBook-Pro.local>
2021-03-09 16:23:06 -0500

    fetchingproducts from react (useEffect)

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../data/products';
+import axios from 'axios';
 
 const Home = () => {
-  return (
+  const [products, setProducts]=useState([]);
+  
+  useEffect(()=>{
+  const fetchProducts= async ()=>{
+    const {data}=await axios.get('/api/v1/products');
+    setProducts(data);
+  }
+  fetchProducts();
+  },[]);
+
+  return(
     <>
       <h1>Lastest products</h1>
       <Row>
