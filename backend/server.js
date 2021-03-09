f37db31a0a8199895c6404228910c8c71391254d
Mauro <mauro@Mauros-MacBook-Pro.local>
2021-03-09 17:01:42 -0500

    se implementa las variable de entorno

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 
 const app = express();
 
@@ -17,4 +20,4 @@ app.get('/api/v1/products/:id', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
