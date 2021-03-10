cb1940d7bd4607a824257991e5a856d08eeda867
Mauro <mauro@Mauros-MacBook-Pro.local>
2021-03-09 20:15:57 -0500

    se hace conexion mongo backend 2

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from 'colors';
+//connect DB
+import connectDB from "./config/db.js"
+
 import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -22,5 +28,5 @@ app.get("/api/v1/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.cyan.bold)
 );
