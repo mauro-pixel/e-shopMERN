4dc21a3fd4658e37e6485e939c2a09216314ba46
Mauro <mauro@Mauros-MacBook-Pro.local>
2021-03-09 20:50:52 -0500

    se hace crea modelos en backend

---
+++ backend/models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+    {
+     user_id:{
+       type:mongoose.Schema.Types.ObjectId,
+       required:true,
+       ref:'User'
+    },
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
+    },
+    {
+      timestamps: true,
+    }
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
